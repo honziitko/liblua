7a98de8a379f64144c6d226cca50d80725524e4e
Honzitko <236162629+honziitko@users.noreply.github.com>
2025-11-29 08:55:20 +0100

    Add #include

--- examples/main.lua
+++ examples/main.lua
@@ -1,13 +1,13 @@
 local liblua = require("liblua")
-local stdarg = require("liblua.stdarg.h")
+liblua.include("stdarg.h")
 
 local function foo(x, y, ...)
-    local ap = stdarg.va_list()
-    stdarg.va_start(ap, "y")
+    local ap = va_list()
+    va_start(ap, "y")
     for _ = 1, 2 do
-        print(stdarg.va_arg(ap, "number"))
+        print(va_arg(ap, "number"))
     end
-    stdarg.va_end(ap)
+    va_end(ap)
 end
 
 foo(1, 2, 67, 41)
--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,27 @@
 local M = {}
+local CALLER = 2
 
-function M.foo()
-    print("Hello, World!")
+local ownedEnvs = {}
+
+local function moveTable(dst, src)
+    for k, v in pairs(src) do
+        dst[k] = v
+    end
+    return dst
+end
+local function copyTable(t)
+    return moveTable({}, t)
+end
+
+function M.include(file)
+    local origEnv = getfenv(CALLER)
+    local env = (ownedEnvs[origEnv] and origEnv) or copyTable(origEnv)
+    local module = require("liblua." .. file)
+    moveTable(env, module)
+    if env ~= origEnv then
+        ownedEnvs[env] = true
+        setfenv(CALLER, env)
+    end
 end
 
 return M
